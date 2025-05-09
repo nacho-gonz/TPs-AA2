a1dd4a180dc055916766950be2609a0f4063538a
Ignacio <nacho.gonzalez02410@gmail.com>
2025-05-09 11:54:02 -0300

    Problema 2
    ultimo script para entrenamiento y cambios en record-dataset

--- Problema2/record-dataset.py
+++ Problema2/record-dataset.py
@@ -1,12 +1,24 @@
-import os
+import mediapipe as mp
+from mediapipe.tasks import python
+from mediapipe.tasks.python import vision
 import cv2
+import os
+import urllib.request
+import numpy as np
+import shutil
+from tensorflow.keras.preprocessing.image import ImageDataGenerator
+from tensorflow.keras.preprocessing.image import img_to_array, load_img
 
 webcam = cv2.VideoCapture(0)
 counter = 1
-
+clases = ['piedra', 'papel', 'tijera']
+n = 0
 print("--- Instrucciones ---")
+print("Las imagenes serán tomadas por clase en el siguiente orden: piedra, papel y tijera")
 print("Presiona 's' para guardar una foto.")
+print("Presiona 'e' para cambiar de clase")
 print("Presiona 'q' para salir.")
+
 
 while True:
     try:
@@ -14,19 +26,28 @@ while True:
         if not check:
             print("No se pudo capturar el frame.")
             break
-
-        cv2.imshow("Capturando (Presiona 's' para guardar, 'q' para salir)", frame)
-
+        
+        cv2.imshow("Capturando (Presiona 's' para guardar, 'e' para cambiar de clase y 'q' para salir)", frame)
+        clase = clases[n]
+        print(f'Capturando la imagenes de la clase {clase}')
         key = cv2.waitKey(1)
 
         if key == ord('s'): 
-            dir = f"./fotos/papel"
-            filename = f"{dir}/papel_{counter}.jpg"
+            dir = f"./fotos/{clase}"
+            filename = f"{dir}/{clase}_{counter}.jpg"
             os.makedirs(dir, exist_ok=True)
             cv2.imwrite(filename, frame)
             print(f"Foto guardada como: {filename}")
             counter += 1 
-
+        elif key == ord('e'):
+            if n >= 2:
+                print('No hay más clases, se procede a terminar el programa')
+                webcam.release()
+                cv2.destroyAllWindows()
+                break
+            n += 1
+            counter = 1
+            print(f'Cambiando a la clase {clase}')
         elif key == ord('q'):
             print("Apagando cámara...")
             webcam.release()
@@ -40,3 +61,110 @@ while True:
         cv2.destroyAllWindows()
         print("Programa terminado.")
         break
+
+
+if 'model.task' not in os.listdir('./'):
+
+    url = 'https://storage.googleapis.com/mediapipe-models/hand_landmarker/hand_landmarker/float16/1/hand_landmarker.task'
+    urllib.request.urlretrieve(url, "model.task")
+
+base_options = python.BaseOptions(model_asset_path='./model.task')
+options = vision.HandLandmarkerOptions(base_options=base_options,
+                                        num_hands=1)
+
+detector = vision.HandLandmarker.create_from_options(options)
+
+
+train_data_dir = './fotos'  # Carpeta de origen de las imágenes
+output_dir = './augmented_images'  # Carpeta para guardar imágenes aumentadas
+os.makedirs(output_dir, exist_ok=True)
+
+datagen = ImageDataGenerator(
+    rotation_range=30,         
+    width_shift_range=0.1,    
+    height_shift_range=0.1,   
+    shear_range=0.2,         
+    zoom_range=0.2,          
+    horizontal_flip=True,     
+    vertical_flip=True,     
+    brightness_range=[0.8, 1.2],  
+    fill_mode='nearest'       
+)
+
+batch_size = 32
+img_width, img_height = 640, 480
+
+train_generator = datagen.flow_from_directory(
+    train_data_dir,
+    target_size=(img_height, img_width),
+    batch_size=batch_size,
+    class_mode='categorical',  
+    save_to_dir=output_dir,    
+    save_prefix='aug',         
+    save_format='jpg'         
+)
+
+augmentation_factor = 3
+
+def augment_and_save(class_folder):
+    class_path = os.path.join(train_data_dir, class_folder)
+    output_class_path = os.path.join(output_dir, class_folder)
+    os.makedirs(output_class_path, exist_ok=True)
+    
+    print(f'Procesando clase: {class_folder}')
+    
+    for img_name in os.listdir(class_path):
+        if not img_name.lower().endswith(('.png', '.jpg', '.jpeg')):
+            continue
+            
+        img_path = os.path.join(class_path, img_name)
+        img = load_img(img_path)  
+        x = img_to_array(img)     
+        x = x.reshape((1,) + x.shape)  
+        
+        # Guardar imagen original
+        original_filename = f'original_{img_name}'
+        img.save(os.path.join(output_class_path, original_filename))
+        
+        # Generar y guardar imágenes aumentadas
+        i = 0
+        for batch in datagen.flow(x, batch_size=1,
+                                 save_to_dir=output_class_path,
+                                 save_prefix='aug',
+                                 save_format='jpeg'):
+            i += 1
+            if i >= augmentation_factor:
+                break  
+
+
+for class_folder in os.listdir(train_data_dir):
+    if os.path.isdir(os.path.join(train_data_dir, class_folder)):
+        augment_and_save(class_folder)
+
+
+
+lista_puntos = []
+lista_labels = []
+ppt = ['papel','piedra','tijera']
+
+
+for gesto in ppt:
+    for i, foto in enumerate(os.listdir(f'./augmented_images/{gesto}')):
+        mp_image = mp.Image.create_from_file(f'./augmented_images/{gesto}/{foto}')
+        hand_landmarker_result = detector.detect(mp_image)
+        try:
+            lista_puntos.append([coord for lm in hand_landmarker_result.hand_landmarks[0] for coord in (lm.x, lm.y)])
+        except:
+            continue
+        
+        if gesto == 'clase_papel':
+            lista_labels.append(1)
+        elif gesto == 'clase_piedra':
+            lista_labels.append(0)
+        else:
+            lista_labels.append(2)
+
+
+
+np.save('./rps_dataset.npy', lista_puntos)
+np.save('./rps_labels.npy', lista_labels)
---
+++ Problema2/train-gesture-classifier.py
@@ -0,0 +1,81 @@
+import matplotlib.pyplot as plt
+import pandas as pd
+import tensorflow as tf
+from tensorflow.keras.layers import Dense
+from pathlib import Path
+from keras.layers import Input, Dropout
+from keras.layers import Dense, Activation
+from tensorflow.keras.utils import to_categorical
+from tensorflow.keras.callbacks import ReduceLROnPlateau, EarlyStopping
+from sklearn.model_selection import train_test_split
+import numpy as np
+
+
+x_data = np.load('./rps_dataset.npy')
+y_data = np.load('./rps_labels.npy')
+
+
+x_temp, x_test, y_temp, y_test = train_test_split(x_data, y_data, test_size=0.2,random_state=123,stratify=y_data)
+
+x_train, x_val, y_train, y_val = train_test_split(x_temp, y_temp, test_size=0.25, random_state=321, stratify=y_temp)
+
+y_train_cat = to_categorical(y_train, num_classes=3)
+y_val_cat = to_categorical(y_val, num_classes=3)
+y_test_cat = to_categorical(y_test, num_classes=3)
+
+# Configurar para que TensorFlow utilice la GPU por defecto
+gpus = tf.config.experimental.list_physical_devices('GPU')
+if gpus:
+    try:
+        # Configurar para que TensorFlow asigne memoria dinámicamente
+        for gpu in gpus:
+            tf.config.experimental.set_memory_growth(gpu, True)
+        # Especificar la GPU por defecto
+        logical_gpus = tf.config.experimental.list_logical_devices('GPU')
+        print(len(gpus), "Physical GPUs,", len(logical_gpus), "Logical GPUs")
+    except RuntimeError as e:
+        # Manejar error
+        print(e)
+
+
+def build_model(input_shape):
+    i = Input(input_shape, dtype=tf.float32)
+
+    x = Dense(32)(i)
+    x = Activation('relu')(x)
+    x = Dropout(0.1)(x)
+
+    x = Dense(16)(x)
+    x = Activation('relu')(x)
+    x = Dropout(0.1)(x)
+    
+    x = Dense(3)(x)
+    output = Activation('softmax')(x)
+
+    return tf.keras.Model(inputs=i, outputs=output)
+
+
+print("Building model")
+model = build_model(input_shape=(x_train.shape[1],))
+
+model.compile(
+    optimizer='adam',
+    loss="categorical_crossentropy",
+    metrics=['accuracy'])
+
+early_stopping = EarlyStopping(monitor="val_loss", patience=10, verbose=0, mode="min", restore_best_weights=True)
+
+reduce_lr = ReduceLROnPlateau(
+    monitor="loss", factor=0.5, patience=8, verbose=1, min_delta=1e-4, mode="min"
+)
+
+history = model.fit(
+    x_train,
+    y_train_cat,
+    validation_data=(x_val, y_val_cat),
+    epochs=100,
+    batch_size=32,
+    callbacks=[reduce_lr, early_stopping],
+)
+
+model.save('rps_model.h5')
